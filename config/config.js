51eb66ce9f1483ed75d681ee06bbd0f2f7093a5c
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-22 16:04:10 -0300

    tenta configuraçao de db

--- config/config.js
+++ config/config.js
@@ -1,24 +1,4 @@
 require('dotenv').config();
-const Sequelize = require('sequelize');
-
-Sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
-
-Sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 module.exports = {
   development: {
